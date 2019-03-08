8b4ffb3ec983cb20b12c2e2fe7de9e4ef86e15a3
JustinChiangkc <38731067+JustinChiangkc@users.noreply.github.com>
2019-03-08 17:39:07 +0800

    Update finalP4.py

--- Final/finalP4.py
+++ Final/finalP4.py
@@ -8,22 +8,17 @@ Created on Sat May 12 15:07:19 2018
 def primes_list(N):
     '''
     N: an integer
+    Return Prime list that samller than N
     '''
     fac = {}
     pri = []
     for x in range(2,N+1):
-        #print('x=',x)
         for y in range(1,x+1):
-            #print('y=',y)
             if x%y == 0:
                 try:
                     fac[x] += [y]
-                    #print('try')
                 except KeyError:
-                    #print('fac[',x,'] =[',y,']')
                     fac[x] =[y]
-                    
-        #print(list(fac.items()))
         if fac[x] == [1,x]:
             pri.append(x)
     return pri
